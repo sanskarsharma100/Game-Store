c76262aa21327f4a20bee6e79cfad2baa4f6a475
sanskarsharma100 <Thebestone@123>
2023-06-06 22:59:22 +0530

    fixed scroll position

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { MainContextProvider } from "./Context/MainContext.jsx";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <MainContextProvider>
+        <ScrollToTop />
         <App />
       </MainContextProvider>
     </BrowserRouter>
